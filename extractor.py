71f207068412f704d9d0ec2593e9d2dace4ccf4c
giorgiopea <annatar93@gmail.com>
2017-01-13 16:55:32 +0100

    fixing

--- extractor.py
+++ extractor.py
@@ -4,22 +4,24 @@ import os
 import csv
 import sys
 
+import re
+
+
 class Extractor:
-    def __init__(self, jobsFile, stagesFile, resultDirectory):
-        self.resultDirectory = resultDirectory
-        self.jobsFile = jobsFile
-        self.stagesFile = stagesFile
+    def __init__(self, top_directory, target_directory):
+        self.top_directory = top_directory
+        self.target_directory = target_directory
         self.stagesRows = None
         self.stagesTasksList = []
         self.stagesCompletionList = []
 
-    def buildstagesCompletionList(self):
-        f = open(self.jobsFile, "r")
+    def buildstagesCompletionList(self,file_):
+        f = open(file_, "r")
         stages = csv.DictReader(f)
         for item in stages:
             self.stagesCompletionList.append({
                 "stageId": item["Stage ID"],
-                "completionTime" : item["Completion Time"]
+                "completionTime": item["Completion Time"]
             })
 
     def mergeList(self):
@@ -27,64 +29,74 @@ class Extractor:
         z = OrderedDict({})
         for item in self.stagesCompletionList:
             for sub_item in self.stagesTasksList:
-                if(item["stageId"] == sub_item["stageId"]):
+                if item["stageId"] == sub_item["stageId"]:
                     z = sub_item.copy()
                     z["completionTime"] = item["completionTime"]
                     targetList.append(z)
         return targetList
 
-    def produceFile(self,finalList):
-        f = open(self.resultDirectory+'/summary.csv','w')
+    def produceFile(self, finalList):
+        f = open(self.target_directory + '/summary.csv', 'w')
         headers = finalList[0].keys()
         writer = csv.writer(f, delimiter=',', lineterminator='\n')
         writer.writerow(headers)
         for item in finalList:
             writer.writerow(item.values())
 
-    def run(self):
-        self.fileValidation(self.stagesFile)
-        f = open(self.stagesFile, "r")
+    def runSingle(self, file_):
+        tasks_file = file_+"/tasks_1.csv"
+        stages_file = file_+"/stages_1.csv"
+        f = open(tasks_file, "r")
         self.stagesRows = self.orderStages(csv.DictReader(f))
         f.close()
         self.buildstagesTasksList()
-        self.buildstagesCompletionList()
+        self.buildstagesCompletionList(stages_file)
         self.produceFile(self.mergeList())
 
+    def run(self):
+        pattern = re.compile('application_([0-9]+)_([0-9]+)_dir')
+        for f in os.listdir(self.top_directory):
+            if pattern.match(f):
+                self.runSingle(self.top_directory+"/"+f)
+
+
+
+
     """Checks the existence of the given file path"""
-    def fileValidation(self,filename):
-        if not(os.path.exists(filename)):
-            print("The file "+filename+" does not exists")
+
+    def fileValidation(self, filename):
+        if not (os.path.exists(filename)):
+            print("The file " + filename + " does not exists")
             exit(-1)
 
     """Orders the stages dict by 'Stage Id'"""
-    def orderStages(self,stages):
-        return sorted(stages, key = lambda x: x["Stage ID"])
 
+    def orderStages(self, stages):
+        return sorted(stages, key=lambda x: x["Stage ID"])
 
-    def computeStagesTasksDetails(self,stageId,batch):
+    def computeStagesTasksDetails(self, stageId, batch):
         shuffleBatch = []
         normalBatch = []
+        bytesBatch = []
         for item in batch:
-            if item[1] == "ResultTask":
-                normalBatch.append(int(item[0]))
-            else:
-                shuffleBatch.append(int(item[0]))
-
-        if(len(shuffleBatch)==0):
-            shuffleBatch.append(-1)
-        if(len(normalBatch)==0):
-            normalBatch.append(-1)
+            normalBatch.append(item[0])
+            shuffleBatch.append(item[1])
+            bytesBatch.append(item[2])
         maxTask = max(normalBatch)
         maxShuffle = max(shuffleBatch)
         avgTask = reduce(lambda x, y: x + y, normalBatch) / len(normalBatch)
         avgShuffle = reduce(lambda x, y: x + y, shuffleBatch) / len(shuffleBatch)
+        maxBytes = max(bytesBatch)
+        avgBytes = reduce(lambda x, y: x + y, bytesBatch) / len(bytesBatch)
         targetDict = OrderedDict({})
         targetDict["stageId"] = stageId
-        targetDict["numTask"] = len(batch)
+        targetDict["nTask"] = len(batch)
         targetDict["maxTask"] = maxTask
         targetDict["avgTask"] = avgTask
-        targetDict["maxShuffle"] = maxShuffle
-        targetDict["avgShuffle"] = avgShuffle
+        targetDict["SHmax"] = maxShuffle
+        targetDict["SHavg"] = avgShuffle
+        targetDict["Bmax"] = maxBytes
+        targetDict["Bavg"] = avgBytes
         return targetDict
 
     def buildstagesTasksList(self):
@@ -92,19 +104,24 @@ class Extractor:
         lastRow = None
         for row in self.stagesRows:
             if lastRow != None and lastRow["Stage ID"] != row["Stage ID"]:
-                self.stagesTasksList.append(self.computeStagesTasksDetails(lastRow["Stage ID"],batch))
+                self.stagesTasksList.append(self.computeStagesTasksDetails(lastRow["Stage ID"], batch))
                 batch = []
-            batch.append([row["Executor Run Time"],row["Task Type"]])
+            if row["Shuffle Write Time"] == "NOVAL":
+                batch.append([int(row["Executor Run Time"]), -1, -1])
+            else:
+                batch.append([int(row["Executor Run Time"]), int(row["Shuffle Write Time"]), int(row["Shuffle Bytes Written"])])
+
             lastRow = row
-        self.stagesTasksList.append(self.computeStagesTasksDetails(lastRow["Stage ID"],batch))
+        self.stagesTasksList.append(self.computeStagesTasksDetails(lastRow["Stage ID"], batch))
+
 
 def main():
     args = sys.argv
-    if len(args) != 4:
-        print("Required args: [STAGES_PER_JOB_CSV_FILE] [TASKS_PER_STAGE_CSV_FILE] [RESULT_DIRECTORY]")
+    if len(args) != 3:
+        print("Required args: [TOP_DIRECTORY] [TARGET_DIRECTORY]")
         exit(-1)
     else:
-        extractor = Extractor(str(args[1]),str(args[2]),str(args[3]))
+        extractor = Extractor(str(args[1]), str(args[2]))
         extractor.run()
 
 
